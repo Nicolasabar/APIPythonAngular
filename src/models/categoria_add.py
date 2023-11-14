cb1d1361df203a5442b850bb6f152821d11996e7
Nicolasabar <abarcanicolas6@gmail.com>
2023-11-14 15:26:39 -0300

    validacion precio actualizado

--- src/models/categoria_add.py
+++ src/models/categoria_add.py
@@ -6,8 +6,9 @@ from pymysql import IntegrityError
 def add_category(nombre, precio, padre_id):
     connection = get_connecction()  # Asumiendo que esta función obtiene una conexión válida
     result = validate_name(nombre)
+    v_price = validate_price(precio)
 
-    if result is True:
+    if result is True and v_price is True:
         try:
             with connection.cursor() as cursor:
                 query = "INSERT INTO categorias (nombre, precio, padre_id) VALUES (%s, %s, %s)"
@@ -23,8 +24,17 @@ def add_category(nombre, precio, padre_id):
     else:
         return f'No se pudo agregar' 
 
+
 def validate_name(nombre):
     if len(nombre) <= 10:
         return True
     else:
         return 'El largo del nombre es incorrecto. Máximo 10 caracteres.'
+    
+
+def validate_price(price):
+    if isinstance(price, int) and len(str(price)) <= 10 and price > 0:
+        return True
+    else:
+        raise ValueError('Ingresar valor numérico de longitud máxima 10 y mayor a 0')
+    
--- src/models/categoria_update.py
+++ src/models/categoria_update.py
@@ -1,21 +1,29 @@
 from . import categoria
 from ..database.db_mysql import get_connecction
+from .categoria_add import validate_name, validate_price
 
 
 def update_category(id, nombre, precio):
     connection = get_connecction()
-    try:
-        with connection.cursor() as cursor:
-            query = """
-                UPDATE categorias
-                SET nombre = %s, 
+
+    # validar largo de nombre al actualizar
+    result = validate_name(nombre)
+    v_price = validate_price(precio)
+    
+    if result is True and v_price is True:
+        
+        try:
+            with connection.cursor() as cursor:
+                query = """
+                    UPDATE categorias
+                    SET nombre = %s, 
                     precio = %s
-                WHERE id = %s;
-            """
-            cursor.execute(query, (nombre, precio, id))  # Corregido: se deben pasar nombre, precio e id
-            connection.commit()
-        return 'Actualizado con éxito'
-    except Exception as e:
-        return f'No se pudo actualizar, error: {e}'
-    finally:
-        connection.close()
+                    WHERE id = %s;
+                """
+                cursor.execute(query, (nombre, precio, id))  # Corregido: se deben pasar nombre, precio e id
+                connection.commit()
+            return 'Actualizado con éxito'
+        except Exception as e:
+            return f'No se pudo actualizar, error: {e}'
+        finally:
+            connection.close()
